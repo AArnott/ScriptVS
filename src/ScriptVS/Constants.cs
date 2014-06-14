31d2b72d6df966600b5c5c64c11e92196dabc924
Igal Tabachnik <hmemcpy@gmail.com>
2014-06-14 16:41:30 +0300

    VSPackage skeleton added
    (based on Clide and VSSDK nugets)

---
+++ src/ScriptVS/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ScriptVS
+{
+    internal static class Constants
+    {
+        public const string PackageGuid = "5208bc3a-63d5-4432-ab77-5a4000fc87b7";
+    }
+}
---
+++ src/ScriptVS/ScriptVSPackage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Clide;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ScriptVS
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [ProvideAutoLoad(UIContextGuids.NoSolution)]
+    [InstalledProductRegistration("ScriptVS", "ScriptCS tools for Visual Studio", "1.0")]
+    [Guid(Constants.PackageGuid)]
+    public sealed class ScriptVSPackage : Package
+    {
+        static ScriptVSPackage()
+        {
+            LocalResolver.Initialize(Path.GetDirectoryName(typeof(ScriptVSPackage).Assembly.Location));
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Host.Initialize(this);
+        }
+    }
+}
