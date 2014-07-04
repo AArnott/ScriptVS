21429e65553bb3492c21541878c53a3b4a5a3d54
Igal Tabachnik <hmemcpy@gmail.com>
2014-07-04 18:19:09 +0300

    It compiles! Ship it!

---
+++ src/ScriptVS/Rules/RuleProperties.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using Microsoft.VisualStudio.ProjectSystem.Utilities;
+
+namespace ScriptVS.Rules
+{
+    [Export]
+    [PartMetadata(ProjectCapabilities.Requires, ProjectCapabilities.CSharp)]
+    internal partial class RuleProperties : StronglyTypedPropertyAccess
+    {
+        [ImportingConstructor]
+        public RuleProperties([Import(RequiredCreationPolicy = CreationPolicy.Shared)] ConfiguredProject configuredProject)
+            : base(configuredProject)
+        {
+        }
+
+        public RuleProperties(ConfiguredProject configuredProject, string file, string itemType, string itemName)
+            : base(configuredProject, file, itemType, itemName)
+        {
+        }
+
+        public RuleProperties(ConfiguredProject configuredProject, IProjectPropertiesContext projectPropertiesContext)
+            : base(configuredProject, projectPropertiesContext)
+        {
+        }
+
+        public RuleProperties(ConfiguredProject configuredProject, UnconfiguredProject unconfiguredProject)
+            : base(configuredProject, unconfiguredProject)
+        {
+        }
+    }
+}
